2ebd6f23b5a5655d4c6ecf9c235a9f4fbfa9a646
faintwind <614587150@qq.com>
2021-06-17 09:57:10 +0800

    完成主页的功能开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,3 +1,15 @@
 $.ajaxPrefilter(function(options){
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function(res){
+        var responseJSON = res.responseJSON;
+        if(responseJSON.status === 1 && responseJSON.message === '身份认证失败！'){
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
